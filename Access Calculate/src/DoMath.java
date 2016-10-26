957247ab850ec30b50571fee904e88cc5a56efb6
jonathanko500 <jkko9010@gmail.com>
2016-10-26 10:52:46 -0700

    split string 2

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -1,7 +1,8 @@
 public class DoMath {
 	public static void main (String []args) {
-		Calculate.cube(2); {
-		}
+		Calculate.toImproperFrac(6, 1, 5); 
+		System.out.println(Calculate.toImproperFrac(6, 1, 5));
 	}
 }
-
+		
+		

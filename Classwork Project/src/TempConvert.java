b845939cd315d18606dedacf1d6a9e21cb04f8bb
jonathanko500 <jkko9010@gmail.com>
2017-08-30 12:14:09 -0700

    nothing

--- Classwork Project/src/TempConvert.java
+++ Classwork Project/src/TempConvert.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 
 public class TempConvert {
 
 	public static void main (String[] args) {
-		double fahrenheit=32;
-		double celsius ;
+		double fahrenheit;
+		double celsius;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a temp: ");
+		fahrenheit = Double.parseDouble(input.nextLine());
 		celsius = (5.0/9.0)*(fahrenheit-32);
 		System.out.println(celsius);
 	}
